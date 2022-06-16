53ee52f59909dd99e47a11569681832f1474e55d
georgi-raychev <105843314+georgi-rayhev@users.noreply.github.com>
2022-06-16 10:49:29 +0300

    Update Crud-Operations.sql

--- Crud-Operations.sql
+++ Crud-Operations.sql
@@ -2,13 +2,13 @@
 --create cusutmer with all fields
 insert into customers(Id,Profile_name,Email,Phone,Age,Gdpr_consent,is_custumer_profile_active,profile_created_at,profile_deactivated,
 reason_for_deactivation,notes) values 
-(11,'Georgi','georgi.raychev@abv.bg','0889345678',22,true,true,CURRENT_TIMESTAMP,null,null,'Notes for the profile');
+('Georgi','georgi.raychev@abv.bg','0889345678',22,true,true,CURRENT_TIMESTAMP,null,null,'Notes for the profile');
 
 --create custumer only with mandatory fields
 insert into customers (Id,Profile_name,email,phone,gdpr_consent,is_custumer_profile_active,profile_created_at) values
-(22,'Ivan','ivan124@abv.bg','0988231243',true,true,CURRENT_TIMESTAMP);
+('Ivan','ivan124@abv.bg','0988231243',true,true,CURRENT_TIMESTAMP);
 
--- retrieves only customer’s name and phone
+-- retrieves only customerâ€™s name and phone
 select Profile_name, phone
 from customers c 
 
@@ -19,23 +19,23 @@ from customers c
 --retrieving all customer info by customer id
 select *
 from customers c 
-where id = 11;
+where id = 1;
 
 --update customer by id - change contact phone 
 update customers 
 set phone = '0888667788'
-where id = 11;
+where id = 1;
 
 --update customer by id - deactivate accout
 update customers 
 set is_custumer_profile_active = false , profile_deactivated = current_timestamp , reason_for_deactivation = 'This is needed in the task'
-where id = 11;
+where id = 1;
 
 --update customer by id - activate accout
 update customers 
 set is_custumer_profile_active = true 
-where id = 11;
+where id = 1;
 
 --delete cutumomer by id
 delete from customers 
-where id = 11;
+where id = 1;
