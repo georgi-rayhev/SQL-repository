2ffd70a496bb7640f25ac144c7dda479ab13a899
georgi-raychev <105843314+georgi-rayhev@users.noreply.github.com>
2022-06-16 09:21:27 +0300

    Add files via upload

---
+++ Database management, data types and constraints.sql
@@ -0,0 +1,23 @@
+create table customers (
+	Id int not null,
+    Profile_name VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Phone VARCHAR(20) NOT NULL UNIQUE,
+    Age INT default '99',
+    Gdpr_consent BOOLEAN NOT NULL,
+    Is_custumer_profile_active BOOLEAN NOT NULL,
+    Profile_created_at TIMESTAMP default CURRENT_TIMESTAMP,
+    Profile_deactivated TIMESTAMP,
+    Reason_for_deactivation VARCHAR(255),
+    Notes VARCHAR(2500)
+);
+
+create index idx_name on customers (Profile_name);  -- creating index for fast searching by name
+
+create database orders_management ; --query for creating data base 
+
+drop database orders_management ; --query for deleting data base 
+
+drop table public.customers ; --query for deleting table
+
+truncate table public.customers ; --query for deleting data from data table
